d9e4e452cded7e16cdc48f0f00ead2b9d28d73bc
Dylan Bochman <DylanBochman@gmail.com>
2025-06-26 19:30:15 -0400

    reconfigured URL

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/JJJennings/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
